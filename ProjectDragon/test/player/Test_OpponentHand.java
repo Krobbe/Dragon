47779a6a2f29a4f7be74f5812469a8175e28e78a
Robin Andersson <k.robin.andersson@gmail.com>
2012-03-23 09:47:41 +0100

    Created test for OpponentHand

---
+++ ProjectDragon/test/player/Test_OpponentHand.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package player;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Robin
+ *
+ */
+public class Test_OpponentHand {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
