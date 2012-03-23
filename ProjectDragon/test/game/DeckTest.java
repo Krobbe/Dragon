4e674fd876adc5a111388a82264614761fddd4a0
Lisa Stenberg <lisast@student.chalmers.se>
2012-03-23 09:52:59 +0100

    Har bytt paket på testerna jag skrivit

---
+++ ProjectDragon/test/game/DeckTest.java
@@ -0,0 +1,27 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import model.game.Card;
+import model.game.Deck;
+
+import org.junit.Test;
+
+public class DeckTest {
+
+	@Test
+	public void testPopCard() {
+		Deck d = new Deck();
+		Card c1, c2;
+		boolean check = true;
+		
+		for(int i = 0; i < 52; i = i+2) {
+			c1 = d.popCard();
+			c2 = d.popCard();
+			
+		}
+		
+		assertTrue(check);
+	}
+
+}
