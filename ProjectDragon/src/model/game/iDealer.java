6816eff748539096da8d026d0d2712ad2b1e6563
Lisa Stenberg <lisast@student.chalmers.se>
2012-03-27 10:36:29 +0200

    En enum HandValueType för alla olika typer av pokerhänder

--- ProjectDragon/src/model/game/iDealer.java
+++ ProjectDragon/src/model/game/iDealer.java
@@ -1,6 +1,16 @@
 package model.game;
 
+/**
+ * An interface that represents a Dealer. 
+ * 
+ * A dealer have at least one deck, but can also have several decks. 
+ * @author lisastenberg
+ *
+ */
 public interface iDealer {
 
+	/**
+	 * @return the top card of the Deck(s).
+	 */
 	public Card popCard();
 }
