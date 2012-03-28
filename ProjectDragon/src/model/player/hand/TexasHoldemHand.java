7421d37990faccdf04de2a0e422fdf112a454780
Robin Andersson <k.robin.andersson@gmail.com>
2012-03-28 16:07:21 +0200

    Added equals, toString and HashCode (standard only, just for good practice) to TexasHoldemHand

--- ProjectDragon/src/model/player/hand/TexasHoldemHand.java
+++ ProjectDragon/src/model/player/hand/TexasHoldemHand.java
@@ -65,4 +65,97 @@ public class TexasHoldemHand implements iHand {
 	public boolean isVisible() {
 		return isVisible;
 	}
+	
+	/**
+	 * Returns a string describing the hand
+	 * 
+	 * @return "'card1 value' of 'card1 suite', 'card2 value' of 'card2 suite' and so on
+	 * ending with 'card3 value' of 'card3 suite'"
+	 * 
+	 */
+	@Override
+	public String toString() {
+		
+		StringBuilder stringB = new StringBuilder();
+		
+		for(int i = 0 ; i < cards.size() -1 ; i++){
+			stringB.append(cards.get(i).toString());
+		}
+		
+		stringB.append(" and " + cards.get(cards.size()-1));
+		
+		return stringB.toString();
+		
+	}	
+	
+	/**
+	 * Determines if this hand is equal to another hand.
+	 * 
+	 * @return Two hands are equal if they are the same object or if their cards are equal
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if(o == null) {
+			return false;
+		}
+		else if(this == o) {
+			return true;
+		}
+		else if (o.getClass() != this.getClass()) {
+			return false;
+		}
+		else if (this.getCards().size() != ((TexasHoldemHand) o).getCards().size()){
+			return false;
+		}
+		
+		/*
+		 * If the classes are the same the cards in each hand is compared
+		 */
+		else {
+			
+			/**TODO Improve equals method in TexasHoldemHand. Perhaps duplicate
+			 * the lists and remove occurences from both lists if a match is
+			 * found.
+			 */
+			
+			List<Card> otherCards = ((TexasHoldemHand) o).getCards();
+			
+			//Every card in this hand is compared to the other Hand's cards
+			for(Card card : cards){
+				
+				boolean sameCards = false;
+				
+				// The loop ends if a match is found or if no match exists
+				int i = 0;
+				while(i < otherCards.size() && !sameCards){
+					
+					// The boolean is set to true if 
+					if(card.equals(otherCards.get(i))){
+						sameCards = true;
+					}
+				
+					i++;
+				}
+				
+				/* If no match is found for this hand's cards then the two hands
+				 * are not equal
+				 */
+				if(!sameCards){
+					return false;
+				}
+
+			}
+			return true;
+
+		}
+		
+	}
+	
+	//Since we at the current state aren't planning on using any hashtables this code was added
+	//for the cause of good practice
+	public int hashCode() {
+		  assert false : "hashCode not designed";
+		  return 44; // any arbitrary constant will do
+	}
+	
 }
