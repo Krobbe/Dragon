ba71651faeae374c90bcbf99f9898bcc6c0ebcca
Lisa Stenberg <lisast@student.chalmers.se>
2012-05-17 14:57:25 +0200

    Nu har vi en fin bakgrundsfärg

---
+++ ProjectDragon/src/client/gui/menu/P.java
@@ -0,0 +1,16 @@
+package client.gui.menu;
+
+import java.awt.Color;
+
+/**
+ * This class holds data for the GUI.
+ * @author lisastenberg
+ *
+ */
+public enum P {
+	INSTANCE;
+	
+	public Color getBackground() {
+		return new Color(188, 219, 204);
+	}
+}
